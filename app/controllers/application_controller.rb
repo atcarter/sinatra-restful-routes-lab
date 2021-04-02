2ac40214c25cefd2e163bcde9f07cccfc7e8af33
Antonio <atcarter005@gmail.com>
2021-04-02 06:05:05 -0500

    commit tree 9e9635090926c9a695befd8df4c115fa6fa0301c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,51 @@ class ApplicationController < Sinatra::Base
 
   # code actions here!
 
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+
+    erb :show
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+    redirect "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find_by_id(params[:id])
+    if @recipe
+      erb :edit
+    else
+      redirect '/recipes/:id'
+    end
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    if @recipe && @recipe.update(name: params[:name], ingredients: params[:ingredients], cook_time: params[:cook_time])
+      redirect "/recipes/#{@recipe.id}"
+    else
+      redirect "/recipes/#{@recipe.id}/edit"
+    end
+  end
+
+  delete '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    if @recipe
+      @recipe.delete
+    end
+    redirect '/recipes'
+  end
+
+
 end
